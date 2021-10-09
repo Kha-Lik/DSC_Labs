0c04af52d0e68345bb65d898007e985c61322221
MykolaV <kolya.vasevich@gmail.com>
2021-10-09 22:24:57 +0300

    [gRPC] Created project for "The Smartest" game

---
+++ TheSmartest/TheSmartest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TheSmartest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
