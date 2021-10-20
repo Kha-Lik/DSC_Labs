7c793101a0c631b8d805562a214bfb871b04308a
Kolia Vaskevych <kolyavaskevich19@gmail.com>
2021-10-20 22:48:44 +0300

    Updated to top-level entrypoint

--- UdpBroadcast/UdpBroadcastClient/Program.cs
+++ UdpBroadcast/UdpBroadcastClient/Program.cs
@@ -1,20 +1,8 @@
 ﻿using System;
-using System.Net;
-using System.Net.Sockets;
-using System.Text;
-using System.Threading.Tasks;
+using UdpBroadcastClient;
 
-namespace UdpBroadcastClient
-{
-    internal static class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.Write("Enter the port to listen on: ");
-            var port = int.Parse(Console.ReadLine() ?? "5555");
+Console.Write("Enter the port to listen on: ");
+var port = int.Parse(Console.ReadLine() ?? "5555");
 
-            var client = new Client(port);
-            client.Start();
-        }
-    }
-}
+var client = new Client(port);
+client.Start();
--- UdpBroadcast/UdpBroadcastServer/Program.cs
+++ UdpBroadcast/UdpBroadcastServer/Program.cs
@@ -1,39 +1,31 @@
 ﻿using System;
+using UdpBroadcastServer;
 
-namespace UdpBroadcastServer
+Console.WriteLine("Enter the port to start on:");
+var port = int.Parse(Console.ReadLine() ?? "6699");
+
+var server = new Server(port, "127.0.0.1");
+server.Init();
+
+bool flag = true;
+while (flag)
 {
-    static class Program
+    Console.WriteLine("==================\n" +
+                      "\t1 - Send message\n" +
+                      "\t2 - Exit\n");
+    var input = Console.ReadLine();
+    switch (input)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Enter the port to start on:");
-            var port = int.Parse(Console.ReadLine() ?? "6699");
-            
-            var server = new Server(port, "127.0.0.1");
-            server.Init();
-
-            bool flag = true;
-            while (flag)
-            {
-                Console.WriteLine("==================\n" +
-                                  "\t1 - Send message\n" +
-                                  "\t2 - Exit\n");
-                var input = Console.ReadLine();
-                switch (input)
-                {
-                    case "1":
-                        Console.WriteLine("Enter message:\n");
-                        var msg = Console.ReadLine();
-                        server.SendMessage(msg);
-                        break;
-                    case "2":
-                        flag = false;
-                        break;
-                    default:
-                        Console.WriteLine("Error! Unexpected input!");
-                        break;
-                }
-            }
-        }
+        case "1":
+            Console.WriteLine("Enter message:\n");
+            var msg = Console.ReadLine();
+            server.SendMessage(msg);
+            break;
+        case "2":
+            flag = false;
+            break;
+        default:
+            Console.WriteLine("Error! Unexpected input!");
+            break;
     }
 }
